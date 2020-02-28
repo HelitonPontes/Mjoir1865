0a3023426314203e6b2f7d50b7571a30147645c8
Heliton Pontes <36651605+HelitonPontes@users.noreply.github.com>
2020-02-28 00:43:31 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mjoir1865
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 0;
+            int n = int.Parse(Console.ReadLine());
+            for (i = 0; n > i; i++)
+            {
+               
+               
+                string[] texto = Console.ReadLine().Split(' ');
+                string nome = texto[0]; int forca = int.Parse(texto[1]);
+
+                if (nome == "Thor")
+                {
+                    Console.WriteLine("Y");
+
+                }
+                else
+                {
+                    Console.WriteLine("N");
+                }
+                
+            }
+            
+            Console.ReadKey();
+
+
+        }
+    }
+}
